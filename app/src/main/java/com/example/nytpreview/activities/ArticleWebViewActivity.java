a9e80f2410f54686ab47c90c7e3e3c2041b25b53
austinwerner <r.austin.werner@gmail.com>
2020-02-11 19:06:12 -0600

    Add share article action
    Add title bar to web view and add share action button.

--- app/src/main/java/com/example/nytpreview/activities/ArticleWebViewActivity.java
+++ app/src/main/java/com/example/nytpreview/activities/ArticleWebViewActivity.java
@@ -1,11 +1,14 @@
 package com.example.nytpreview.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 import com.example.nytpreview.R;
 
@@ -13,18 +16,26 @@ public class ArticleWebViewActivity extends AppCompatActivity {
 
     final public static String BUNDLE_URL = "ARTICLE_URL";
 
+    String mUrl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_web_view);
 
+        Toolbar toolbar = findViewById(R.id.action_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+
         Bundle bundle = getIntent().getExtras();
         String url = bundle.getString(BUNDLE_URL);
 
         if( url != null ) {
+            mUrl = url;
             loadUrl(url);
         }
         else {
+            // Invalid URL, pop the page
             finish();
         }
     }
@@ -42,5 +53,27 @@ public class ArticleWebViewActivity extends AppCompatActivity {
             view.loadUrl(url);
             return true;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.article_share_action, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        shareLink();
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void shareLink() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, mUrl);
+        sendIntent.setType("text/plain");
+
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
     }
 }
